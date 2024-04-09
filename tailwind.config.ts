f375df7191c31f64cbc291a422e8be052afdbb96
Claudio Bispo <claudio_cssp@hotmail.com>
2024-04-09 15:04:54 -0300

    criacao dos assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,33 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        orelega: ['FonteOrelega', 'sans-serif'],
+        sen: ['FonteSen', 'sans-serif'],
+        manrope: ['FonteManrope', 'sans-serif'],
+      },
+      backgroundColor: {
+        primeira: "#47667B",
+        segunda: "#6D8B89",
+        terceira: "#A5C3A7",
+        quarta: "#D5E0B5",
+        quinta: "#F8F4C4",
+        sexta: "#FFFFFF",
+      },
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
