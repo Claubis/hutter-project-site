4986bd91cc76c89ccf27dcf96c699c637a8cdb45
Claudio Bispo <claudio_cssp@hotmail.com>
2024-04-11 14:43:08 -0300

    ajuste form feedback

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,44 +1,11 @@
 'use client'
-import Link from "next/link";
+
 import Image from "next/image";
-import { useState } from "react";
+
 
 export default function Contact(){
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-
-    const encode = (data:any) => {
-        return Object.keys(data)
-          .map(
-            (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
-          )
-          .join('&');
-      };
     
-    const handleSubmit = (e:any) => {
-    const data = {name, email, message};
-
-    fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encode({ 'form-name': 'contact-form', ...data }),
-    })
-        .then(() => alert('Success!'))
-        .catch((error) => alert(error));
-
-    e.preventDefault();
-    };
-
-    // Função atualizada para lidar com mudanças nos campos do formulário
-    const handleChange = (e: any) => {
-        const { name, value } = e.target;
-        if (name === 'name') setName(value);
-        if (name === 'email') setEmail(value);
-        if (name === 'message') setMessage(value);
-  };
-
     return(
 
         <section className="bg-primeira">
@@ -59,11 +26,10 @@ export default function Contact(){
 
                         <form 
                         name="form-contact"
-                        method="POST"
+                        method="post"
                         data-netlify="true"
-                        netlify-honeypot="bot-field"
                         className="self-stretch space-y-3"
-                        onSubmit={handleSubmit}>
+                        >
 
                         <input type="hidden" name="form-contact" value="contact"/>
                             
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 
 import HomePage from "./HomePage/page";
 import About from "./About/page";
